3ab9a8d4077cf3228060e7a16af9f10e7b868465
Hung Vu <vunamhung@gmail.com>
2019-09-02 15:23:17 +0700

    add global vars

---
+++ .backup/index.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @var WP_Query $wp_query
+ */
+global $wp_query;
+
+/**
+ * @var WP_Post $post
+ */
+global $post;
+
+/**
+ * @var WP_Customize_Manager $wp_customize
+ */
+global $wp_customize;
+
+/**
+ * @var wpdb $wpdb
+ */
+global $wpdb;
+
+/**
+ * @var WP_Comment $comment
+ */
+global $comment;
+
+/**
+ * @var WP_Styles $wp_styles
+ */
+global $wp_styles;
+
+/**
+ * @var WP_User $authordata
+ */
+global $authordata;
+
+/**
+ * @var Custom_Image_Header $custom_image_header
+ */
+global $custom_image_header;
+
+/**
+ * @var WP_Scripts $wp_scripts
+ */
+global $wp_scripts;
+
+/**
+ * @var WP_Scripts $scripts
+ */
+global $scripts;
+
+/**
+ * @var WP_Admin_Bar $wp_admin_bar
+ */
+global $wp_admin_bar;
+
+/**
+ * @var WP_Roles $wp_roles
+ */
+global $wp_roles;
+
+/**
+ * @var WP $wp
+ */
+global $wp;
+
+/**
+ * @var WP_Filesystem_Base $wp_filesystem
+ */
+global $wp_filesystem;
