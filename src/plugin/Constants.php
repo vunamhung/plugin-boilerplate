5ea9ffbbb0f45340f64ffa0726a6aa1cd38531c3
Hung Vu <vunamhung@gmail.com>
2019-11-28 15:59:15 +0700

    remove constant

--- src/plugin/Constants.php
+++ src/plugin/Constants.php
@@ -10,7 +10,6 @@ define(__NAMESPACE__ . '\PLUGIN_URI', 'vnh_uri');
 define(__NAMESPACE__ . '\PLUGIN_VERSION', 'vnh_version');
 define(__NAMESPACE__ . '\PLUGIN_TEXT_DOMAIN', 'vnh_slug');
 define(__NAMESPACE__ . '\PLUGIN_AUTHOR', 'vnh_author');
-define(__NAMESPACE__ . '\PLUGIN_AUTHOR_NAME', 'vnh_author_name');
 define(__NAMESPACE__ . '\PLUGIN_AUTHOR_URI', 'vnh_author_uri');
 define(__NAMESPACE__ . '\PLUGIN_DOCUMENT_URI', 'vnh_document_uri');
 define(__NAMESPACE__ . '\PLUGIN_SLUG', Plugin::$info['slug']);
