203de3c1cee13af92b0568bbd2f08f55a8071c10
Hung Vu <vunamhung@gmail.com>
2020-07-13 10:24:19 +0700

    custom tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
 	purge: ["./src/assets/js/settings_page/**/*.js", "./src/assets/js/frontend/**/*.js"],
-	theme: {},
+	theme: {
+		extend: {
+			screens: {
+				"2xl": "1440px",
+			},
+			spacing: {
+				"72": "18rem",
+				"84": "21rem",
+				"96": "24rem",
+			},
+		},
+	},
 	variants: {},
 	plugins: [],
 };
