546093fbbc3e8f7d9534504d29f2265f5f8ac5d9
Hung Vu <vunamhung@gmail.com>
2020-07-13 21:31:05 +0700

    :recycle: refactor code

--- src/api/Setting_API.php
+++ src/api/Setting_API.php
@@ -2,33 +2,29 @@
 
 namespace vnh_namespace\api;
 
-use vnh\contracts\Bootable;
+use vnh\REST_Controller;
 use vnh_namespace\Settings;
+use WP_REST_Server;
 use const vnh_namespace\PLUGIN_SLUG;
 
-class Setting_API implements Bootable {
-	public function boot() {
-		add_action('rest_api_init', [$this, 'register_routes']);
-	}
+class Setting_API extends REST_Controller {
+	public $prefix = PLUGIN_SLUG;
+	public $route = '/settings';
 
 	/**
 	 * @uses update_settings, permissions, get_settings
 	 */
 	public function register_routes() {
-		register_rest_route(PLUGIN_SLUG . '/v1', '/settings', [
-			'methods' => 'POST',
+		register_rest_route($this->namespace, $this->route, [
+			'methods' => WP_REST_Server::CREATABLE,
 			'callback' => [$this, 'update_settings'],
 			'permissions_callback' => [$this, 'permissions'],
 		]);
-		register_rest_route(PLUGIN_SLUG . '/v1', '/settings', [
-			'methods' => 'GET',
+		register_rest_route($this->namespace, $this->route, [
+			'methods' => WP_REST_Server::READABLE,
 			'callback' => [$this, 'get_settings'],
 			'permissions_callback' => [$this, 'permissions'],
 		]);
-	}
-
-	public function permissions() {
-		return current_user_can('manage_options');
 	}
 
 	public function update_settings(\WP_REST_Request $request) {
@@ -42,5 +38,9 @@ class Setting_API implements Bootable {
 
 	public function get_settings(\WP_REST_Request $request) {
 		return rest_ensure_response(Settings::get_settings());
+	}
+
+	protected function permissions() {
+		return current_user_can('manage_options');
 	}
 }
