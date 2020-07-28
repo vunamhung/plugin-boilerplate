951264121d63b33135e35ae86de84a186d986c85
Hung Vu <vunamhung@gmail.com>
2020-07-28 23:32:24 +0700

    :recycle: use function and hook

--- src/assets/js/settings_page/App.js
+++ src/assets/js/settings_page/App.js
@@ -1,104 +1,59 @@
-import { BaseControl, Button, PanelBody, PanelRow, Placeholder, Spinner, ToggleControl } from "@wordpress/components";
-import apiFetch from "@wordpress/api-fetch";
+import { useEffect, useState } from "@wordpress/element";
+import { BaseControl, Button, PanelBody, PanelRow, ToggleControl } from "@wordpress/components";
+import isEmpty from "lodash/isEmpty";
+import { saveSettings, useSettings } from "./helpers";
 
-class App extends Component {
-	state = {
-		isAPILoaded: false,
-		isAPISaving: false,
-		analytics_key: "",
-		analytics_status: false,
-	};
+export default function App() {
+	const [loading, settings, setSettings] = useSettings();
+	const { analytics_key, analytics_status } = settings;
+	const [saving, setSaving] = useState(false);
 
-	componentDidMount() {
-		apiFetch({ path: pluginApiPath }).then((settings) => {
-			this.setState({
-				analytics_key: settings.analytics_key,
-				analytics_status: settings.analytics_status,
-				isAPILoaded: true,
-			});
-		});
-	}
+	useEffect(() => {
+		!isEmpty(settings) && saveSettings(settings);
+	}, [saving]);
 
-	render() {
-		if (!this.state.isAPILoaded) {
-			return (
-				<>
-					<div className="bg-white py-6 mb-4">
-						<div className="container w-9/12 lg:w-7/12">
-							<div className="flex items-center">
-								<h1>{pluginName}</h1>
-								<div className="ml-3 text-xs rounded font-light bg-neutral-200 px-1 border-neutral-300">v{pluginVersion}</div>
-							</div>
-						</div>
-					</div>
-					<div className="container w-9/12 lg:w-7/12 flex items-center">
-						<Placeholder>
-							<Spinner />
-						</Placeholder>
-					</div>
-				</>
-			);
-		}
-
-		return (
-			<>
-				<div className="bg-white py-6 mb-4">
-					<div className="container w-9/12 lg:w-7/12">
-						<div className="flex items-center">
-							<h1>{pluginName}</h1>
-							<div className="ml-3 text-xs rounded font-light bg-neutral-200 px-1 border-neutral-300">v{pluginVersion}</div>
-						</div>
-					</div>
-				</div>
-
+	return (
+		<>
+			<div className="bg-white py-6 mb-4">
 				<div className="container w-9/12 lg:w-7/12">
-					<PanelBody title={__("Settings", "vnh_textdomain")} className="bg-white mb-4">
-						<PanelRow>
-							<BaseControl
-								label={__("Google Analytics Key", "vnh_textdomain")}
-								help={__("In order to use Google Analytics, you need to use an API key.", "vnh_textdomain")}
-								id="options-google-analytics-api"
-							>
-								<input
-									type="text"
-									id="options-google-analytics-api"
-									value={this.state.analytics_key}
-									placeholder={__("Google Analytics API Key", "vnh_textdomain")}
-									disabled={this.state.isAPISaving}
-									onChange={(e) => this.setState({ analytics_key: e.target.value })}
-								/>
-							</BaseControl>
-						</PanelRow>
-						<PanelRow>
-							<ToggleControl
-								label={__("Track Admin Users?", "vnh_textdomain")}
-								help={__("Would you like to track views of logged-in admin accounts?.", "vnh_textdomain")}
-								checked={this.state.analytics_status}
-								onChange={() => this.setState({ analytics_status: !this.state.analytics_status })}
-							/>
-						</PanelRow>
-					</PanelBody>
-					<Button isPrimary isLarge disabled={this.state.isAPISaving} onClick={this.saveSettings}>
-						{__("Save Settings", "vnh_textdomain")}
-					</Button>
+					<div className="flex items-center">
+						<h1>{pluginName}</h1>
+						<div className="ml-3 text-xs rounded font-light bg-neutral-200 px-1 border-neutral-300">v{pluginVersion}</div>
+					</div>
 				</div>
-			</>
-		);
-	}
+			</div>
 
-	saveSettings = () => {
-		this.setState({ isAPISaving: true });
-
-		const data = {
-			analytics_key: this.state.analytics_key,
-			analytics_status: this.state.analytics_status,
-		};
-
-		apiFetch({ path: pluginApiPath, method: "POST", parse: false, data }).then(() => {
-			this.setState({ isAPISaving: false });
-			console.log("Settings saved");
-		});
-	};
+			<div className="container w-9/12 lg:w-7/12">
+				<PanelBody title={__("Settings", "vnh_textdomain")} className="bg-white mb-4">
+					<PanelRow>
+						<BaseControl
+							label={__("Google Analytics Key", "vnh_textdomain")}
+							help={__("In order to use Google Analytics, you need to use an API key.", "vnh_textdomain")}
+							id="options-google-analytics-api"
+						>
+							<input
+								type="text"
+								id="options-google-analytics-api"
+								value={analytics_key}
+								onChange={(e) => setSettings({ ...settings, ...{ analytics_key: e.target.value } })}
+								disabled={loading}
+								placeholder={__("Google Analytics API Key", "vnh_textdomain")}
+							/>
+						</BaseControl>
+					</PanelRow>
+					<PanelRow>
+						<ToggleControl
+							label={__("Track Admin Users?", "vnh_textdomain")}
+							help={__("Would you like to track views of logged-in admin accounts?.", "vnh_textdomain")}
+							checked={analytics_status}
+							onChange={() => setSettings({ ...settings, ...{ analytics_status: !analytics_status } })}
+						/>
+					</PanelRow>
+				</PanelBody>
+				<Button isPrimary isLarge disabled={loading} onClick={() => setSaving(true)}>
+					{__("Save Settings", "vnh_textdomain")}
+				</Button>
+			</div>
+		</>
+	);
 }
-
-export default App;
---
+++ src/assets/js/settings_page/helpers.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "@wordpress/element";
+import apiFetch from "@wordpress/api-fetch";
+
+export function useSettings() {
+	const [settings, setSettings] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setLoading(true);
+		apiFetch({ path: pluginApiPath }).then((settings) => {
+			setSettings(settings);
+			setLoading(false);
+			console.log("Settings loaded");
+		});
+	}, []);
+
+	return [loading, settings, setSettings];
+}
+
+export function saveSettings(data) {
+	apiFetch({ path: pluginApiPath, method: "POST", parse: false, data }).then(() => {
+		console.log("Settings saved");
+	});
+}
