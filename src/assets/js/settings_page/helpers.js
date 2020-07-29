08fccbc97f30465623a2b03ac484f3a4cc113d60
Hung Vu <vunamhung@gmail.com>
2020-07-29 21:01:26 +0700

    Click outside element custom hook

--- src/assets/js/settings_page/helpers.js
+++ src/assets/js/settings_page/helpers.js
@@ -1,6 +1,22 @@
 import { useEffect, useState } from "@wordpress/element";
 import apiFetch from "@wordpress/api-fetch";
 
+export function useOutsideClick(ref, callback) {
+	const handleClick = (e) => {
+		if (ref.current && !ref.current.contains(e.target)) {
+			callback();
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener("click", handleClick);
+
+		return () => {
+			document.removeEventListener("click", handleClick);
+		};
+	});
+}
+
 export function useSettings() {
 	const [settings, setSettings] = useState([]);
 	const [loading, setLoading] = useState(true);
